db5c85696bbc5063d5579300721465a3738ef706
Yogurt <nguyenday3332@gmail.com>
2024-07-06 20:12:42 +0700

    Section 47

--- src/test/message_queue/rabbitmq/consumer.js
+++ src/test/message_queue/rabbitmq/consumer.js
@@ -6,7 +6,7 @@ async function consume() {
   try {
     const connection = await amqp.connect("amqp://guest:12345@localhost");
     const channel = await connection.createChannel();
-    await channel.assertQueue(queue, { durable: false });
+    await channel.assertQueue(queue, { durable: true });
     console.log(`[*] Waiting for messages in ${queue}. To exit press CTRL+C`);
     channel.consume(
       queue,
--- src/test/message_queue/rabbitmq/producer.js
+++ src/test/message_queue/rabbitmq/producer.js
@@ -7,7 +7,7 @@ async function produce() {
   try {
     const connection = await amqp.connect("amqp://guest:12345@localhost");
     const channel = await connection.createChannel();
-    await channel.assertQueue(queue, { durable: false });
+    await channel.assertQueue(queue, { durable: true });
     channel.sendToQueue(queue, Buffer.from(message));
     console.log(`[x] Sent '${message}'`);
     await channel.close();
