8e0b993784f177f4f362145f8eda00a722488d09
Yogurt <nguyenday3332@gmail.com>
2024-07-15 01:30:25 +0700

    Section 73: modified minio

--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import CloudinaryController from "../controllers/cloudinary.controller.js";
-// import MinioController from "../controllers/minio.controller.js";
+import MinioController from "../controllers/minio.controller.js";
 import AWSS3Controller from "../controllers/aws.s3.controller.js";
 import { upload } from "../helpers/multer.helper.js";
 import { UploadValidate } from "../helpers/validate.helper.js";
@@ -19,17 +19,17 @@ UploadRouter.delete(
   validate(UploadValidate.deleteImage, ["query", "body"]),
   controller(CloudinaryController.deleteImage)
 );
-// UploadRouter.post(
-//   "/minio/file",
-//   upload.single("file"),
-//   validate(UploadValidate.singleFile, "file"),
-//   controller(MinioController.uploadFile)
-// );
-// UploadRouter.delete(
-//   "/minio/file",
-//   validate(UploadValidate.deleteFile, ["query", "body"]),
-//   controller(MinioController.deleteFile)
-// );
+UploadRouter.post(
+  "/minio/file",
+  upload.single("file"),
+  validate(UploadValidate.singleFile, "file"),
+  controller(MinioController.uploadFile)
+);
+UploadRouter.delete(
+  "/minio/file",
+  validate(UploadValidate.deleteFile, ["query", "body"]),
+  controller(MinioController.deleteFile)
+);
 
 UploadRouter.post(
   "/aws/s3/file",
