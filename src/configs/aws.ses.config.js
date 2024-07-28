a83f96cbc6b83b7743071ac6a2083723e2dff6b0
Yogurt <nguyenday3332@gmail.com>
2024-07-28 09:42:48 +0700

    OK

--- src/configs/aws.ses.config.js
+++ src/configs/aws.ses.config.js
@@ -3,12 +3,7 @@ import * as aws from "@aws-sdk/client-ses";
 import env from "./env.config.js";
 
 const ses = new aws.SES(env.cloud.ses);
-<<<<<<< HEAD
-
-// create Nodemailer SES transporter
-=======
 // Test comman
->>>>>>> b69207e (Initial commit)
 const transporter = nodemailer.createTransport({
   SES: { ses, aws },
 });
